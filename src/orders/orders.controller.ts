fe373e81dfc2d59f5649ee8f2e48c27b3be417fb
Mattias Duarte <duartemattias3@gmail.com>
2025-02-24 21:44:32 -0400

    agregando comunicacion y gestion con el orders ms

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { NATS_SERVICE } from 'src/config/services';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+
+@Controller('orders')
+export class OrdersController {
+
+  constructor(
+    @Inject(NATS_SERVICE) private readonly ordersClient: ClientProxy
+  ) { }
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send("createOrder", createOrderDto)
+      .pipe(
+        catchError(error => {
+          throw new RpcException(error)
+        })
+      )
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersClient.send("findAllOrders", {})
+      .pipe(
+        catchError(error => {
+          throw new RpcException(error)
+        })
+      )
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ordersClient.send("findOneOrder", id)
+      .pipe(
+        catchError(error => {
+          throw new RpcException(error)
+        })
+      )
+  }
+}
